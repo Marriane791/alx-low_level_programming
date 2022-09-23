495e5ad5497ac3a3e23bc970e12206c95d9052a4
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-23 23:38:20 +0300

    [fix]Correction

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,13 +2,13 @@
 
 /**
  * A hash function implementing the djb2 algorithm.
- * @str: string to be hashed 
- * Return the calculated hash
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
+	unsigned long int hash;
 	int c;
 
 	hash = 5381;
